617c46b8ea121c6f15522aa366f4087dd99213a2
jozef kubis <kubis.jozef@outlook.com>
2026-06-19 14:09:48 +0200

    refactor: remove unused components and assets; add basic HomeScreen and installation instructions

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>ERC-RN-APP</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#295dd7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
