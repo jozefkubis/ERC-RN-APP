4f1dda68443a6924aa966c28714a352794f6e292
jozef kubis <kubis.jozef@outlook.com>
2026-06-19 14:21:50 +0200

    refactor: simplify layout structure and enhance HomeScreen with StatusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,5 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return (
-    <>
-      <Stack screenOptions={{ headerShown: false }} />
-      <StatusBar style="auto" />
-    </>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,26 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>ERC-RN-APP</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.text}>ERC-RN-APP</Text>
+      </View>
+    </>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#295dd7',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#295dd7",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
